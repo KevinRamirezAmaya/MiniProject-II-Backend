c07c5c228ba1c1abdbbf5cdae738ea6711700f32
Gabriela Guzmán <136855829+gabrielaguzmn@users.noreply.github.com>
2025-10-14 14:19:38 -0500

    test using gmail service

--- api/services/EmailService.ts
+++ api/services/EmailService.ts
@@ -9,10 +9,7 @@ interface EmailResponse {
 
 const createTransporter = (): nodemailer.Transporter => {
     return nodemailer.createTransport({
-
-        host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-        port: parseInt(process.env.EMAIL_PORT || '587'),
-        secure: process.env.EMAIL_SECURE === 'true', // true for 465, false for other ports
+        service: 'gmail',
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
